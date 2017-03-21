b0e4a6a47bcddfb22395a4d633e4f40d4f46feff
DimitarChristoff <dimitar.christoff1@rbs.com>
2017-03-21 17:27:19 +0000

    wrong export name for selection plugin

--- plugins/index.js
+++ plugins/index.js
@@ -3,7 +3,7 @@ export CellCopyManager from './slick.cellcopymanager';
 export CellRangeDecorator from './slick.cellrangedecorator';
 export CellRangeSelector from './slick.cellrangeselector';
 export CellSelectionModel from './slick.cellselectionmodel';
-export CheckboxSelectColum from './slick.checkboxselectcolumn';
+export CheckboxSelectColumn from './slick.checkboxselectcolumn';
 export HeaderButtons from './slick.headerbuttons';
 export HeaderMenu from './slick.headermenu';
 export RowMoveManager from './slick.rowmovemanager';
