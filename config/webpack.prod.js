bc73b73fe05c8a11f51dc01d382c5443a6cbaa9f
Dimitar Christoff <christoff@gmail.com>
2018-07-19 15:19:02 +0100

    closes #38 and makes it occlude interactjs issues (#39)

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -34,7 +34,8 @@ module.exports = {
   entry: {
     index: [
       path.join(__INPUT__, 'index.js')
-    ]
+    ],
+    // 'slick.grid.css': path.join(__INPUT__, 'slick.build.scss')
   },
 
   output: {
@@ -56,7 +57,7 @@ module.exports = {
       }
     }, {
       test: /\.(scss|css)$/,
-      loader: 'style-loader!css-loader!scss-loader'
+      loader: 'style-loader!css-loader!sass-loader'
     }, {
       test: /\.(jpe?g|png|gif|svg)$/i,
       loaders: [
